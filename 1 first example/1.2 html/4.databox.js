302d73a4c6953882b6f1f6e912cd16e55f40a526
chanwon moon <kogoome@kakao.com>
2022-04-27 13:59:35 +0900

    함수 용도에 따라 중첩

--- 1 first example/1.2 html/4.databox.js
+++ 1 first example/1.2 html/4.databox.js
@@ -24,36 +24,37 @@ function createStatementData(invoice, plays) {
     result.amount = amountFor(result)
     result.volumeCredits = volumeCreditsFor(result)
     return result
-  }
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID]
-  }
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
+
+    function playFor(aPerformance) {
+      return plays[aPerformance.playID]
     }
-    return result
-  }
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result
+    function amountFor(aPerformance) {
+      let result = 0;
+      switch (aPerformance.play.type) {
+        case "tragedy":
+          result = 40000;
+          if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
+          }
+          break;
+        case "comedy":
+          result = 30000;
+          if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
+          }
+          result += 300 * aPerformance.audience;
+          break;
+        default:
+          throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
+      }
+      return result
+    }
+    function volumeCreditsFor(aPerformance) {
+      let result = 0;
+      result += Math.max(aPerformance.audience - 30, 0);
+      if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+      return result
+    }
   }
   function totalAmount(data) {
     // let result = 0;
@@ -81,7 +82,7 @@ function statement(invoice, plays) {
 // 출력문장
 function renderPlainText(data) {
   let result = `청구내역(고객명: ${data.customer}) \n`
-  result+=data.performances.reduce((str, p)=>
+  result += data.performances.reduce((str, p)=>
     str +` ${p.play.name}: ${usd(p.amount)} (${p.audience}석)\n`,""
   )
   result += `총액: ${usd(data.totalAmount)}\n`
