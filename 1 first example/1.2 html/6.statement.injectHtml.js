e416fa6d85bce1056ab5f16071d4b0a92f4f9862
chanwon moon <kogoome@kakao.com>
2022-04-27 21:01:32 +0900

    정리

--- 1 first example/1.2 html/6.statement.injectHtml.js
+++ 1 first example/1.2 html/6.statement.injectHtml.js
@@ -1,5 +1,6 @@
 import createStatementData from "./5.createStatementData.js";
 import {invoice, plays} from "./5.controller.js";
+
 /*
 * 초기 코드에 비해 라인 수는 배는 늘어났지만
 * 전체 로직을 구성하는 요소 각각이 더 뚜렷이 부각되고
@@ -15,8 +16,8 @@ function statement(invoice, plays) {
 
 function renderPlainText(data) {
   let result = `청구내역(고객명: ${data.customer}) \n`
-  result += data.performances.reduce((str, p)=>
-    str +` ${p.play.name}: ${usd(p.amount)} (${p.audience}석)\n`,""
+  result += data.performances.reduce((str, p) =>
+    str + ` ${p.play.name}: ${usd(p.amount)} (${p.audience}석)\n`, ""
   )
   result += `총액: ${usd(data.totalAmount)}\n`
   result += `적립포인트: ${data.totalVolumeCredits} 점\n`
@@ -27,14 +28,15 @@ function renderHtml(data) {
   let result = `<h1> 청구내역(고객명: ${data.customer}) </h1>\n`
   result += `<table>\n`
   result += `<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>`
-  result += data.performances.reduce((str, p)=>
-    str +`<tr><td>${p.play.name}</td><td>(${p.audience}석)</td><td>${usd(p.amount)}</td></tr>\n`,""
+  result += data.performances.reduce((str, p) =>
+    str + `<tr><td>${p.play.name}</td><td>(${p.audience}석)</td><td>${usd(p.amount)}</td></tr>\n`, ""
   )
   result += `</table>\n`
   result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`
   result += `<p>적립포인트: <em>${data.totalVolumeCredits}</em> 점</p>\n`
   return result;
 }
+
 function usd(aNumber) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
