280b304455ce56eb5d5257bb3704f9ace8d6bf12
chanwon moon <kogoome@kakao.com>
2022-05-11 13:42:57 +0900

    2장 캡슐화 예제

--- 2 principal of refactoring/ex.capsule.js
+++ 2 principal of refactoring/ex.capsule.js
@@ -1,4 +1,4 @@
-class Capsule {
+class ExCapsule {
   #id
   name // 형식상 표기, 자바스크립트에서는 표기 안해도 무방
   constructor(name) {
@@ -11,10 +11,10 @@ class Capsule {
   getConsole() {this.#console()}
 }
 
-const one = new Capsule("hello")
-console.log(one) // Capsule { name: 'hello' }
+const one = new ExCapsule("hello")
+console.log(one) // ExCapsule { name: 'hello' }
 one.name = "world"
-console.log(one) // Capsule { name: 'world' }
+console.log(one) // ExCapsule { name: 'world' }
 console.log(one.id) // 1
 try {one.console() // ide 밑줄
 } catch (e) {console.log(e) /* TypeError: one.console is not a function*/}
