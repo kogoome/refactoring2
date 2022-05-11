c3722f558bf9f6fc663963af475a8b7822d53c4b
chanwon moon <kogoome@kakao.com>
2022-05-11 13:43:04 +0900

    2장 싱글톤 예제

--- 2 principal of refactoring/ex.singleton.js
+++ 2 principal of refactoring/ex.singleton.js
@@ -1,18 +1,18 @@
-class Singleton {
+class ExSingleton {
   static #instance
   constructor() {
-    if(Singleton.#instance) {
+    if(ExSingleton.#instance) {
       console.log("two")
-      return Singleton.#instance
+      return ExSingleton.#instance
     }
     this.id = 1
     this.text = "hello"
-    Singleton.#instance = this
+    ExSingleton.#instance = this
   }
 }
 
-const one  = new Singleton()
-const two  = new Singleton()
+const one  = new ExSingleton()
+const two  = new ExSingleton()
 console.log(one===two) // true
 console.log(one.instance) // undefined
 // 인스턴스 호출도 이뤄지지 않는다.
